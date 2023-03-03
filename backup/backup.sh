82361f0d298e5f06c5c92872e7361ab1a2cb18f2
h-takesg <82633007+h-takesg@users.noreply.github.com>
2023-03-04 01:00:55 +0900

    rsyncできるようにした

--- backup/backup.sh
+++ backup/backup.sh
@@ -4,3 +4,6 @@ DATE=`date "+%Y%m%d%H%M"`
 
 # generate backup file
 pg_dump --file ./data/$DATE.sql
+
+# sync ./data
+rsync -az -e "ssh -i ~/.ssh/private_key" ./data/ $BACKUP_USER@$BACKUP_HOST:~/kJuiceApp-backup/
