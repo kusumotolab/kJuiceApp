4cf64237c52edd9bd12746f6d730e3663a57e804
Haur514 <haruka.yoshioka.514@gmail.com>
2023-03-11 16:43:50 +0900

    Settingから画像を送信できるようにした

--- src/Setting/User/IconSetting/IconSetting.tsx
+++ src/Setting/User/IconSetting/IconSetting.tsx
@@ -3,49 +3,70 @@ import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import Select from "react-select";
 
-const fetchMemberList = async (setMemberList) =>{
-    const inputdata = await fetch(`${window.location.protocol}//${window.location.host}${window.location.pathname}backend/member`, {
-        method: 'GET',
-        mode: 'cors'
-    })
-    .then(res => res.json())
-    .then(members => {
-        setMemberList(members.filter((member) => {
-            return member.active;
-        }));
-    });
-}
-
-const fetchBase64Img = async (setBase64Img, userId: string) => {
+const fetchMemberList = async (setMemberList) => {
   const inputdata = await fetch(
-    `${window.location.protocol}//${window.location.host}${window.location.pathname}backend/member/image?name=${userId}`,
+    `${window.location.protocol}//${window.location.host}${window.location.pathname}backend/member`,
     {
       method: "GET",
       mode: "cors",
     }
   )
-    .then((res) => res.text())
-    .then((items) => {
-      setBase64Img(items);
+    .then((res) => res.json())
+    .then((members) => {
+      setMemberList(
+        members.filter((member) => {
+          return member.active;
+        })
+      );
     });
 };
 
-function setMemberSelectOptionsFromMemberList(setMemberSelectOptions, memberList){
-    let ret = [];
-    console.log(memberList);
-    console.log(memberList[0]);
-    for(let member in memberList){
-        let tmp = {
-            value: memberList[member]["name"],
-            label: memberList[member]["displayName"]
-        }
-        console.log(member);
-        ret.push(tmp)
-    }
-    console.log(ret);
-    setMemberSelectOptions(ret);
-}
+const submitIconImage = async (fileObject: File, userId: string) => {
+  if (fileObject == undefined) {
+    alert("画像ファイルを選択してください")
+    return;
+  }
+  if (userId == "") {
+    alert("ユーザを選択してください");
+    return;
+  }
 
+  const file = new FormData();
+  file.append("image", fileObject);
+  file.append("userId", userId);
+
+  let url: string = `${window.location.protocol}//${window.location.host}${window.location.pathname}backend/member/image/upload?userId=${userId}`;
+
+  try {
+    await axios.post(url, file).then(function(response) {
+      console.log(response);
+    });
+  } catch (error) {
+    alert(
+      "ファイルの送信に失敗しました．ファイルサイズ/ファイル形式を確認してください．ファイルサイズは10MB以下である必要があります．"
+    );
+    console.log("ファイルのアップに失敗しました");
+  }
+};
+
+function setMemberSelectOptionsFromMemberList(
+  setMemberSelectOptions,
+  memberList
+) {
+  let ret = [];
+  console.log(memberList);
+  console.log(memberList[0]);
+  for (let member in memberList) {
+    let tmp = {
+      value: memberList[member]["name"],
+      label: memberList[member]["displayName"],
+    };
+    console.log(member);
+    ret.push(tmp);
+  }
+  console.log(ret);
+  setMemberSelectOptions(ret);
+}
 
 function IconSetting() {
   const [profileImage, setProfileImage] = useState("");
@@ -54,26 +75,35 @@ function IconSetting() {
 
   const [userId, setUserId] = useState<string>("");
 
-  const [memberList,setMemberList] = useState([{name:"",displayName:"",umpayedAmount:0,attribute:"m1",active:false}]);
+  const [memberList, setMemberList] = useState([
+    {
+      name: "",
+      displayName: "",
+      umpayedAmount: 0,
+      attribute: "m1",
+      active: false,
+    },
+  ]);
 
   const [memberSelectOptions, setMemberSelectOptions] = useState([]);
 
   useEffect(() => {
     fetchMemberList(setMemberList);
-  },[])
+  }, []);
 
   useEffect(() => {
-    setMemberSelectOptionsFromMemberList(setMemberSelectOptions,memberList);
-  },[memberList])
+    setMemberSelectOptionsFromMemberList(setMemberSelectOptions, memberList);
+  }, [memberList]);
 
-  let fileObject: File;
+//   let fileObject: File;
+  const [fileObject,setFileObject] = useState<File>();
 
   const onFileInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (!e.target.files) return;
-    fileObject = e.target.files[0];
-    setProfileImage(window.URL.createObjectURL(fileObject));
+    // fileObject = e.target.files[0];
+    setFileObject(e.target.files[0]);
 
-    handleSubmit(fileObject);
+    setProfileImage(window.URL.createObjectURL(e.target.files[0]));
   };
 
   const onTextInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -82,6 +112,7 @@ function IconSetting() {
 
   const handleSubmit = async (fileObject: File) => {
     if (fileObject == undefined) {
+        alert("画像が選択されていません");
       return;
     }
     if (userId == "") {
@@ -106,28 +137,27 @@ function IconSetting() {
 
   return (
     <IconSettingPane>
-        <IconSettingTitle>
-            ユーザアイコンの設定
-        </IconSettingTitle>
-        <ImagePreview src={profileImage} />
-        <input
-          type="file"
-          multiple
-          accept="image/*"
-          onChange={onFileInputChange}
-        />
-        <Select options={memberSelectOptions}/>
-        <button
-          onClick={() => {
-            console.log(userId);
-            if (userId == "") {
-              return;
-            }
-            fetchBase64Img(setBase64Img, userId);
-          }}
-        >
-            送信する
-        </button>
+      <IconSettingTitle>ユーザアイコンの設定</IconSettingTitle>
+      <ImagePreview src={profileImage} />
+      <input
+        type="file"
+        multiple
+        accept="image/*"
+        onChange={onFileInputChange}
+      />
+      <Select
+        options={memberSelectOptions}
+        onChange={(target) => {
+          setUserId(target.value);
+        }}
+      />
+      <button
+        onClick={() => {
+          handleSubmit(fileObject);
+        }}
+      >
+        送信する
+      </button>
     </IconSettingPane>
   );
 }
@@ -135,24 +165,24 @@ function IconSetting() {
 const IconSettingPane = styled.div`
   color: greenyellow;
   font-size: 2em;
-  color:#303F43;
+  color: #303f43;
   margin: 1em 1em;
-  background-color: #EDEFF1;
-  border-radius: .5em;
-  padding: .5em;
+  background-color: #edeff1;
+  border-radius: 0.5em;
+  padding: 0.5em;
 `;
 
 const ImagePreview = styled.img`
   width: 5em;
   height: 5em;
   border-radius: 2.5em;
-  margin: .5em 0em;
+  margin: 0.5em 0em;
   border: 1px solid;
 `;
 
 const IconSettingTitle = styled.div`
-  position:relative;
-  border-bottom: .1em solid #303F43;
-`
+  position: relative;
+  border-bottom: 0.1em solid #303f43;
+`;
 
 export default IconSetting;
