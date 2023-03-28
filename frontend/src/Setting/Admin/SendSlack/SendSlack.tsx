069c74f6368a41a62db8313127d0bd59d1b1836d
Haur514 <haruka.yoshioka.514@gmail.com>
2023-03-28 08:34:52 +0000

    adminを選択できるようにした

--- frontend/src/Setting/Admin/SendSlack/SendSlack.tsx
+++ frontend/src/Setting/Admin/SendSlack/SendSlack.tsx
@@ -1,59 +1,64 @@
 import { useEffect, useState } from "react";
 import styled from "styled-components";
 import { Backend } from "util/Backend";
+import Select from "react-select";
 import { Button } from "../../../component/Button";
+import { Member } from "types";
 
 function SendSlack() {
-  const [slackMessage,setSlackMessage] = useState<string>("");
+  const [slackMessage, setSlackMessage] = useState<string>("");
+  const [memberList,setMemberList] = useState<Member[]>([]);
+  const [admin,setAdmin] = useState<string>("---");
 
-  async function fetchSlackMessage(){
-    const message = await Backend.getInvokeMessageForSlack("yosiok",1);
-    if(message != null){
+  async function fetchSlackMessage() {
+    const message = await Backend.getInvokeMessageForSlack(admin, 1);
+    if (message != null) {
       setSlackMessage(message);
     }
   }
 
+  async function fetchMemberList() {
+    const memberList = await Backend.getMemberList();
+
+    if (memberList === null) {
+      console.error("fetchMemberList: failed");
+      return;
+    }
+
+    setMemberList(memberList.filter((member) => member.active));
+  }
+
   useEffect(() => {
     fetchSlackMessage();
-  },[]);
+    fetchMemberList();
+  }, [admin]);
+  
+  const options = memberList.map(({ displayName }) => ({
+    value: displayName,
+    label: displayName,
+  }));
 
   return (
     <SendSlackPane>
       メッセージプレビュー
       <MessagePreview>{slackMessage}</MessagePreview>
-      <label>
-        名前：
-        <input type="text" name="name" />
-      </label>
-      <label>
-        属性：
-        <select name="new-user-attribute" defaultValue="teature">
-          <option value="teature">Teature</option>
-          <option value="m2">M2</option>
-          <option value="m1">M1</option>
-          <option value="b4">B4</option>
-        </select>
-      </label>
-      <label>
-        <Button
-          color="gray"
-          onClick={() => {
-            console.log("OK");
-          }}
-          fontColor="white"
-        >
-          キャンセル
-        </Button>
-        <Button
-          color="blue"
-          onClick={() => {
-            console.log("OK");
-          }}
-          fontColor="white"
-        >
-          追加
-        </Button>
-      </label>
+
+      <Select
+        options={options}
+        defaultValue={{ label: "ユーザを選択してください", value: "" }}
+        onChange={(target) => {
+          setAdmin(target?.value ?? "");
+        }}
+        />
+      <Button
+        color="blue"
+        onClick={() => {
+          console.log("OK");
+        }}
+        fontColor="white"
+      >
+        送信
+      </Button>
     </SendSlackPane>
   );
 }
