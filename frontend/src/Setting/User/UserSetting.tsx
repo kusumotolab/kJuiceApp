f18f1d628ef9317422258f7493e63d4df684ba25
grtw2116 <taru0116@gmail.com>
2023-04-01 20:58:00 +0900

    不要なコンポーネントを削除

--- frontend/src/Setting/User/UserSetting.tsx
+++ frontend/src/Setting/User/UserSetting.tsx
@@ -7,14 +7,4 @@ function UserSetting() {
   );
 }
 
-// const UserSettingPane = styled.div`
-//   background-color: #ddffdd;
-//   position: absolute;
-//   top: 6em;
-//   bottom: 0em;
-//   left: 0em;
-//   right: 0em;
-//   font-size: 1em;
-// `;
-
 export { UserSetting };
