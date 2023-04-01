3736e85d8b9a49f80d6692488f645e0dc9efc2e7
grtw2116 <taru0116@gmail.com>
2023-04-01 16:37:07 +0900

    設定画面内のタブをChakra UIで再実装

--- frontend/src/Setting/SettingPane.tsx
+++ frontend/src/Setting/SettingPane.tsx
@@ -1,22 +1,24 @@
-import { Tab, TabList, TabPanel, Tabs } from "react-tabs";
 import { AdminPane } from "./Admin/AdminPane";
 import { UserSetting } from "./User/UserSetting";
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
 
 function SettingPane() {
-  return (
-    <Tabs>
-      <TabList>
-        <Tab>User</Tab>
-        <Tab>Admin</Tab>
-      </TabList>
-      <TabPanel>
-        <UserSetting />
-      </TabPanel>
-      <TabPanel>
-        <AdminPane />
-      </TabPanel>
-    </Tabs>
-  );
+    return (
+        <Tabs>
+            <TabList>
+                <Tab>User</Tab>
+                <Tab>Admin</Tab>
+            </TabList>
+            <TabPanels>
+                <TabPanel>
+                    <UserSetting />
+                </TabPanel>
+                <TabPanel>
+                    <AdminPane />
+                </TabPanel>
+            </TabPanels>
+        </Tabs>
+    );
 }
 
 export { SettingPane };
