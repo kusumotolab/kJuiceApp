d0f12e210d9ccb3e79fe2c9174401ba15d41bed6
grtw2116 <taru0116@gmail.com>
2023-04-01 20:58:56 +0900

    ユーザの削除画面をChakra UIで再実装

--- frontend/src/Setting/Admin/UserDelete/UserDeletePane.tsx
+++ frontend/src/Setting/Admin/UserDelete/UserDeletePane.tsx
@@ -1,85 +1,89 @@
 import { useEffect, useState } from "react";
-import { Button } from "../../../component/Button";
-import { Toggle } from "../../../component/Toggle";
+// import { Button } from "../../../component/Button";
+// import { Toggle } from "../../../component/Toggle";
 import { Member } from "types";
 import { Backend } from "util/Backend";
+import { Table, TableContainer, Tbody, Th, Switch, Thead, Tr, Button } from "@chakra-ui/react";
 
 function UserDeletePane() {
-  const [memberList, setMemberList] = useState<Member[]>([]);
+    const [memberList, setMemberList] = useState<Member[]>([]);
 
-  async function fetchMemberList() {
-    const memberList = await Backend.getMemberList();
+    async function fetchMemberList() {
+        const memberList = await Backend.getMemberList();
 
-    if (memberList === null) {
-      console.error("fetchMemberList: failed");
-      return;
+        if (memberList === null) {
+            console.error("fetchMemberList: failed");
+            return;
+        }
+
+        setMemberList(memberList);
     }
 
-    setMemberList(memberList);
-  }
-
-  async function switchMemberActivity(name: string, activity: boolean) {
-    if (!(await Backend.setMemberActivity(name, activity))) {
-      console.error("switchMemberActivity: failed");
-      return;
+    async function switchMemberActivity(name: string, activity: boolean) {
+        if (!(await Backend.setMemberActivity(name, activity))) {
+            console.error("switchMemberActivity: failed");
+            return;
+        }
+        memberList.findIndex((member) => member.name === name);
+        setMemberList(
+            memberList.map((member) => {
+                if (member.name === name) member.active = activity;
+                return member;
+            })
+        );
     }
-    memberList.findIndex((member) => member.name === name);
-    setMemberList(
-      memberList.map((member) => {
-        if (member.name === name) member.active = activity;
-        return member;
-      })
+
+    async function deleteMember(member: string) {
+        if (!(await Backend.deleteMember(member)))
+            console.error("deleteMember: failed");
+    }
+
+    useEffect(() => {
+        fetchMemberList();
+    }, []);
+return (
+        <div className="UserDelete">
+            <TableContainer>
+                <Table variant="simple">
+                    <Thead>
+                        <Tr className="caption">
+                            <Th>ユーザ名</Th>
+                            <Th>属性</Th>
+                            <Th>Active/InActive</Th>
+                            <Th>削除ボタン</Th>
+                        </Tr>
+                    </Thead>
+                    <Tbody>
+                        {memberList.map((member) => (
+                            <Tr key={member.name}>
+                                <Th>{member.displayName}</Th>
+                                <Th>{member.attribute}</Th>
+                                <Th>
+                                    {/* TODO: member.activeを設定 */}
+                                    <Switch
+                                        isChecked={member.active}
+                                        onChange={() =>
+                                            switchMemberActivity(member.name, !member.active)
+                                        }
+                                    />
+                                </Th>
+                                <Th>
+                                    <Button
+                                        onClick={async () => {
+                                            await deleteMember(member.name);
+                                            await fetchMemberList();
+                                        }}
+                                    >
+                                        削除
+                                    </Button>
+                                </Th>
+                            </Tr>
+                        ))}
+                    </Tbody>
+                </Table>
+            </TableContainer>
+        </div >
     );
-  }
-
-  async function deleteMember(member: string) {
-    if (!(await Backend.deleteMember(member)))
-      console.error("deleteMember: failed");
-  }
-
-  useEffect(() => {
-    fetchMemberList();
-  }, []);
-
-  return (
-    <div className="UserDelete">
-      <table border={1}>
-        <tr className="caption">
-          <th>ユーザ名</th>
-          <th>属性</th>
-          <th>Active/InActive</th>
-          <th>削除ボタン</th>
-        </tr>
-        {memberList.map((member) => (
-          <tr key={member.name}>
-            <th>{member.displayName}</th>
-            <th>{member.attribute}</th>
-            <th>
-              <Toggle
-                onClick={(activity: boolean) =>
-                  switchMemberActivity(member.name, activity)
-                }
-                toggled={member.active}
-              />
-            </th>
-            <th>
-              <Button
-                color="gray"
-                radius="0.5em"
-                onClick={async () => {
-                  await deleteMember(member.name);
-                  await fetchMemberList();
-                }}
-                fontColor="white"
-              >
-                削除
-              </Button>
-            </th>
-          </tr>
-        ))}
-      </table>
-    </div>
-  );
 }
 
 export { UserDeletePane };
