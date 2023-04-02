f5daf749f6749d47cba3b79eb12b199778eb0896
h-takesg <82633007+h-takesg@users.noreply.github.com>
2023-04-02 11:39:25 +0000

    未使用テストコードを一時除外

--- backend/src/test/java/io/db/HistoryDataBaseTest.java
+++ backend/src/test/java/io/db/HistoryDataBaseTest.java
@@ -1,27 +1,27 @@
 package io.db;
 
-import com.github.springtestdbunit.DbUnitTestExecutionListener;
-import com.github.springtestdbunit.annotation.DbUnitConfiguration;
-import jp.ac.osaka_u.ist.sdl.kjuiceapp.repository.MemberRepository;
-import org.dbunit.IDatabaseTester;
-import org.dbunit.database.IDatabaseConnection;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestExecutionListeners;
-import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
-import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+// import com.github.springtestdbunit.DbUnitTestExecutionListener;
+// import com.github.springtestdbunit.annotation.DbUnitConfiguration;
+// import jp.ac.osaka_u.ist.sdl.kjuiceapp.repository.MemberRepository;
+// import org.dbunit.IDatabaseTester;
+// import org.dbunit.database.IDatabaseConnection;
+// import org.springframework.boot.test.context.SpringBootTest;
+// import org.springframework.test.context.TestExecutionListeners;
+// import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+// import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
 
-@SpringBootTest(
-    classes = {MemberRepository.class},
-    webEnvironment = SpringBootTest.WebEnvironment.NONE)
-@DbUnitConfiguration(dataSetLoader = CsvDataSetLoader.class)
-@TestExecutionListeners({
-  DependencyInjectionTestExecutionListener.class,
-  TransactionalTestExecutionListener.class,
-  DbUnitTestExecutionListener.class
-})
+// @SpringBootTest(
+//     classes = {MemberRepository.class},
+//     webEnvironment = SpringBootTest.WebEnvironment.NONE)
+// @DbUnitConfiguration(dataSetLoader = CsvDataSetLoader.class)
+// @TestExecutionListeners({
+//   DependencyInjectionTestExecutionListener.class,
+//   TransactionalTestExecutionListener.class,
+//   DbUnitTestExecutionListener.class
+// })
 public class HistoryDataBaseTest {
-  // TODO
-  // データベースのmockがうまくいかない
-  static IDatabaseTester databaseTester;
-  static IDatabaseConnection connection;
+  // // TODO
+  // // データベースのmockがうまくいかない
+  // static IDatabaseTester databaseTester;
+  // static IDatabaseConnection connection;
 }
