4dceb5071040de60519fd9572343c050c57c08a6
h-takesg <82633007+h-takesg@users.noreply.github.com>
2023-04-06 16:56:37 +0000

    テスト実装準備

--- backend/src/test/java/jp/ac/osaka_u/ist/sdl/kjuiceapp/controller/ItemsControllerTest.java
+++ backend/src/test/java/jp/ac/osaka_u/ist/sdl/kjuiceapp/controller/ItemsControllerTest.java
@@ -7,8 +7,12 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.database.rider.core.api.dataset.DataSet;
+import com.github.database.rider.core.api.dataset.ExpectedDataSet;
+import com.github.database.rider.junit5.api.DBRider;
 import java.util.List;
-import java.util.Map;
+import jp.ac.osaka_u.ist.sdl.kjuiceapp.controller.item.requestbody.ItemAddRequestBody;
+import jp.ac.osaka_u.ist.sdl.kjuiceapp.controller.item.responsebody.ItemResponseBody;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -16,68 +20,21 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
-@Transactional
+@DBRider
+@DataSet(cleanBefore = true)
 @AutoConfigureMockMvc
 @ActiveProfiles("test") // To use test db
 public class ItemsControllerTest {
   @Autowired private MockMvc mockMvc;
-
   @Autowired private ObjectMapper objectMapper;
 
   @Test
-  void normalAddItem() throws Exception {
-    var requestParams =
-        Map.of(
-            "id", "gogotea",
-            "name", "午後の紅茶",
-            "sellingPrice", 80,
-            "costPrice", 60,
-            "category", "juice");
-    String requestBody = objectMapper.writeValueAsString(requestParams);
-
+  @DataSet(value = "ItemsController/normalGetAllItems/before.yaml")
+  public void normalGetAllItems() throws Exception {
     var expectedResponseParams =
-        Map.of(
-            "id",
-            "gogotea",
-            "name",
-            "午後の紅茶",
-            "sellingPrice",
-            80,
-            "costPrice",
-            60,
-            "category",
-            "juice",
-            "isActive",
-            false);
-    String expectedResponseBody = objectMapper.writeValueAsString(expectedResponseParams);
-
-    this.mockMvc
-        .perform(post("/items").contentType(MediaType.APPLICATION_JSON).content(requestBody))
-        .andDo(print())
-        .andExpect(status().isCreated())
-        .andExpect(content().json(expectedResponseBody, true));
-  }
-
-  @Test
-  public void getAllItems() throws Exception {
-    var expectedResponseParams =
-        List.of(
-            Map.of(
-                "id",
-                "cola",
-                "name",
-                "コーラ",
-                "sellingPrice",
-                100,
-                "costPrice",
-                90,
-                "category",
-                "juice",
-                "isActive",
-                true));
+        List.of(new ItemResponseBody("cola", "コーラ", 100, 90, "juice", true));
     String expectedJson = objectMapper.writeValueAsString(expectedResponseParams);
 
     this.mockMvc
@@ -85,5 +42,21 @@ public class ItemsControllerTest {
         .andDo(print())
         .andExpect(status().isOk())
         .andExpect(content().json(expectedJson, true));
+  }
+
+  @Test
+  @ExpectedDataSet(value = "ItemsController/normalAddItem/expected.yaml")
+  void normalAddItem() throws Exception {
+    var requestParams = new ItemAddRequestBody("gogotea", "午後の紅茶", 80, 60, "juice");
+    String requestBody = objectMapper.writeValueAsString(requestParams);
+
+    var expectedResponseParams = new ItemResponseBody("gogotea", "午後の紅茶", 80, 60, "juice", false);
+    String expectedResponseBody = objectMapper.writeValueAsString(expectedResponseParams);
+
+    this.mockMvc
+        .perform(post("/items").contentType(MediaType.APPLICATION_JSON).content(requestBody))
+        .andDo(print())
+        .andExpect(status().isCreated())
+        .andExpect(content().json(expectedResponseBody, true));
   }
 }
