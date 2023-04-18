4331e87975942e862cb86a4b2b1ffe4fb3201d37
grtw2116 <taru0116@gmail.com>
2023-04-18 12:25:39 +0900

    フォーマット

--- frontend/src/HomePage/Item/Food/FoodPane.tsx
+++ frontend/src/HomePage/Item/Food/FoodPane.tsx
@@ -6,48 +6,46 @@ import { Dispatch, SetStateAction } from "react";
 import { Box, Heading, Flex } from "@chakra-ui/react";
 
 type Props = {
-  foodList: Item[];
-  setSelectedItem: Dispatch<SetStateAction<Item>>;
-  selectedMember: Member | null;
-  onOpen: () => void;
-  setPopUpVisibility: Dispatch<SetStateAction<boolean>>;
-  logoDictionary: LogoDictionary;
+    foodList: Item[];
+    setSelectedItem: Dispatch<SetStateAction<Item>>;
+    selectedMember: Member | null;
+    onOpen: () => void;
+    setPopUpVisibility: Dispatch<SetStateAction<boolean>>;
+    logoDictionary: LogoDictionary;
 };
 
 function FoodPane({
-  foodList,
-  setSelectedItem,
-  selectedMember,
-  onOpen,
-  setPopUpVisibility,
-  logoDictionary,
+    foodList,
+    setSelectedItem,
+    selectedMember,
+    onOpen,
+    setPopUpVisibility,
+    logoDictionary,
 }: Props) {
-  return (
-    <Box>
-      <Heading>食品</Heading>
-        <Flex gap={4}>
-        {foodList
-          .sort((a, b) => -a.salesFigure + b.salesFigure)
-          .map((food) => {
-            return (
-              <ItemCard
-                color="#FFC039"
-                onClick={() => {
-                  setSelectedItem(food);
-                  if (selectedMember !== null) {
-                    setPopUpVisibility(true);
-                  }
-                }}
-                name={food.name}
-                item={food}
-                imgSrc={logoDictionary[food.name]}
-                key={food.name}
-              />
-            );
-          })}
-      </Flex>
-    </Box>
-  );
+    return (
+        <Box>
+            <Heading>食品</Heading>
+            <Flex gap={4}>
+                {foodList
+                    .sort((a, b) => -a.salesFigure + b.salesFigure)
+                    .map((food) => {
+                        return (
+                            <ItemCard
+                                color="#FFC039"
+                                onClick={() => {
+                                    setSelectedItem(food);
+                                    onOpen.call(null);
+                                }}
+                                name={food.name}
+                                item={food}
+                                imgSrc={logoDictionary[food.name]}
+                                key={food.name}
+                            />
+                        );
+                    })}
+            </Flex>
+        </Box>
+    );
 }
 
 export { FoodPane };
--- frontend/src/HomePage/Item/purchase/PopUpMenu/PopUpMenu.tsx
+++ frontend/src/HomePage/Item/purchase/PopUpMenu/PopUpMenu.tsx
@@ -1,107 +1,109 @@
 import "./PopUpMenu.css";
-import styled from "styled-components";
-import { SelectCancelPurchaseButtonPane } from "./component/SelectCancelPurchaseButtonPane";
-import { UserInformationPane } from "./component/UserInformationPane";
-import { ItemInformationPane } from "./component/ItemInformationPane";
 import { Backend } from "util/Backend";
 import { Dispatch, SetStateAction, useRef } from "react";
 import { Item, Member } from "types";
 
 import {
-  Button,
-  AlertDialog,
-  AlertDialogBody,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogContent,
-  AlertDialogOverlay,
-  AlertDialogCloseButton,
+    Button,
+    AlertDialog,
+    AlertDialogBody,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogContent,
+    AlertDialogOverlay,
+    AlertDialogCloseButton,
+    useToast,
 } from '@chakra-ui/react'
 
 type Props = {
-  isOpen: boolean;
-  onClose: () => void;
-  setPopUpVisibility: Dispatch<SetStateAction<boolean>>;
-  imgSrc: string;
-  selectedItem: Item | null;
-  setSumPurchased: Dispatch<SetStateAction<number>>;
-  selectedMember: Member | null;
-  setUpdate: Dispatch<SetStateAction<boolean>>;
-  update: boolean;
-  setShowCompleteMessage: Dispatch<SetStateAction<boolean>>;
+    isOpen: boolean;
+    onClose: () => void;
+    setPopUpVisibility: Dispatch<SetStateAction<boolean>>;
+    imgSrc: string;
+    selectedItem: Item | null;
+    setSumPurchased: Dispatch<SetStateAction<number>>;
+    selectedMember: Member | null;
+    setUpdate: Dispatch<SetStateAction<boolean>>;
+    update: boolean;
+    setShowCompleteMessage: Dispatch<SetStateAction<boolean>>;
 };
 
 function PopUpMenu({
-  isOpen,
-  onClose,
-  setPopUpVisibility,
-  imgSrc,
-  selectedItem,
-  setSumPurchased,
-  selectedMember,
-  setUpdate,
-  update,
-  setShowCompleteMessage,
+    isOpen,
+    onClose,
+    setPopUpVisibility,
+    imgSrc,
+    selectedItem,
+    setSumPurchased,
+    selectedMember,
+    setUpdate,
+    update,
+    setShowCompleteMessage,
 }: Props) {
 
-  const cancelRef = useRef<HTMLButtonElement>(null);
+    const toast = useToast();
+    const cancelRef = useRef<HTMLButtonElement>(null);
 
-  async function purchaseItem() {
-    if (selectedMember === null || selectedItem === null) {
-      return;
+    async function purchaseItem() {
+        if (selectedMember === null || selectedItem === null) {
+            return;
+        }
+
+        if (!(await Backend.purchase(selectedMember.name, selectedItem.name)))
+            console.error("purchaseItem: failed");
+
+        setSumPurchased((prev) => prev + 1);
     }
 
-    if (!(await Backend.purchase(selectedMember.name, selectedItem.name)))
-      console.error("purchaseItem: failed");
+    function showToast() {
+        toast({
+            title: "購入完了",
+            description: "購入が完了しました",
+            status: "success",
+            duration: 3000,
+            isClosable: true,
+        });
+    }
 
-    setSumPurchased((prev) => prev + 1);
-  }
+    return (
+        <AlertDialog
+            motionPreset="slideInBottom"
+            leastDestructiveRef={cancelRef}
+            onClose={onClose}
+            isOpen={isOpen}
+            isCentered
+        >
+            <AlertDialogOverlay />
 
-  function closePopUp() {
-    setPopUpVisibility(false);
-  }
-
-  return (
-    <AlertDialog
-        motionPreset="slideInBottom"
-        leastDestructiveRef={cancelRef}
-        onClose={onClose}
-        isOpen={isOpen}
-        isCentered
-    >
-        <AlertDialogOverlay />
-
-        <AlertDialogContent>
-            <AlertDialogHeader>〇〇さん</AlertDialogHeader>
-            <AlertDialogCloseButton />
-            <AlertDialogBody>
-                を購入しますか？
-            </AlertDialogBody>
-            <AlertDialogFooter>
-                <Button colorScheme="teal" variant="outline" ref={cancelRef} onClick={onClose}>キャンセル</Button>
-                <Button colorScheme="teal" variant="solid" onClick={onClose} ml="3">購入</Button>
-            </AlertDialogFooter>
-        </AlertDialogContent>
-    </AlertDialog>
-    // <Background>
-    //   {/* 2段階でvisibilityを設定している
-    //   このコンポーネント自体を表示するかしないかで分岐するべき */}
-    //   <div className={`popup-menu ${visibility ? "visible" : "hidden"}`}>
-    //     <UserInformationPane selectedMember={selectedMember} />
-    //     <ItemInformationPane imgSrc={imgSrc} selectedItem={selectedItem} />
-    //     <SelectCancelPurchaseButtonPane
-    //       purchaseItem={purchaseItem}
-    //       selectedMember={selectedMember}
-    //       selectedItem={selectedItem}
-    //       setSumPurchased={setSumPurchased}
-    //       closePopUp={closePopUp}
-    //       setUpdate={setUpdate}
-    //       update={update}
-    //       setShowCompleteMessage={setShowCompleteMessage}
-    //     />
-    //   </div>
-    // </Background>
-  );
+            <AlertDialogContent>
+                <AlertDialogHeader>{selectedMember?.displayName + "さん"}</AlertDialogHeader>
+                <AlertDialogCloseButton />
+                <AlertDialogBody>{selectedItem?.name + "を購入しますか？"}</AlertDialogBody>
+                <AlertDialogFooter>
+                    <Button
+                        colorScheme="teal"
+                        variant="outline"
+                        ref={cancelRef}
+                        onClick={onClose}
+                    >
+                        キャンセル
+                    </Button>
+                    <Button
+                        colorScheme="teal"
+                        variant="solid"
+                        onClick={() => {
+                            purchaseItem();
+                            showToast();
+                            onClose.call(null);
+                        }}
+                        ml="3"
+                    >
+                        購入
+                    </Button>
+                </AlertDialogFooter>
+            </AlertDialogContent>
+        </AlertDialog>
+    );
 }
 
 export { PopUpMenu };
