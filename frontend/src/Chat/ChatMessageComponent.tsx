a719065d463e4656b0fd8e57ff8ece84def62225
h-takesg <82633007+h-takesg@users.noreply.github.com>
2023-05-05 15:23:26 +0000

    chatのbootstrapをchakraで置き換え

--- frontend/src/Chat/ChatMessageComponent.tsx
+++ frontend/src/Chat/ChatMessageComponent.tsx
@@ -1,19 +1,16 @@
 import "./ChatMessageComponent.css";
 import DefaultIcon from "./../image/userimg/defaultimg.png";
-import { Backend } from "util/Backend";
 import { Chat } from "types";
-import { Dispatch, SetStateAction } from "react";
 
 type Props = {
   chat: Chat;
-  setLastUpdated: Dispatch<SetStateAction<string>>;
+  deleteMessage: (id: number) => Promise<void>;
 };
 
-function ChatMessageComponent({ chat, setLastUpdated }: Props) {
-  const fetchChatDelete = async (id: number) => {
-    if (!(await Backend.deleteChat(id)))
-      console.error("fetchChatDelete: failed");
-  };
+function ChatMessageComponent({ chat, deleteMessage }: Props) {
+  function handleDelete() {
+    deleteMessage(chat.id);
+  }
 
   return (
     <div className="ChatMessageComponent">
@@ -24,26 +21,7 @@ function ChatMessageComponent({ chat, setLastUpdated }: Props) {
           <div className="ChatMessageDate">{chat.date}</div>
           <button
             className="DeleteButton"
-            onClick={async () => {
-              await fetchChatDelete(chat.id);
-              const date = new Date();
-              setLastUpdated(
-                // TODO 日付フォーマットできるライブラリを使う
-                date.getFullYear() +
-                  "/" +
-                  ("0" + (date.getMonth() + 1)).slice(-2) +
-                  "/" +
-                  ("0" + date.getDate()).slice(-2) +
-                  " " +
-                  ("0" + date.getHours()).slice(-2) +
-                  ":" +
-                  ("0" + date.getMinutes()).slice(-2) +
-                  ":" +
-                  ("0" + date.getSeconds()).slice(-2) +
-                  "." +
-                  date.getMilliseconds()
-              );
-            }}
+            onClick={handleDelete}
           >
             Delete
           </button>
