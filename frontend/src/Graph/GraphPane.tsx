f1edd38158e96a7d9779e2348b2b3a0bf2fd645b
h-takesg <82633007+h-takesg@users.noreply.github.com>
2023-05-05 15:22:24 +0000

    一旦Graphを削除

--- frontend/src/Graph/GraphPane.tsx
+++ frontend/src/Graph/GraphPane.tsx
@@ -1,35 +1,8 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import { Tabs } from "react-tabs";
-// import { Tab, TabList, TabPanel, Tabs } from "react-tabs";
-import "react-tabs/style/react-tabs.css";
-// import { SellingBarGraph } from "./selling/SellingBarGraph";
-// import { LineChart } from "./linechart/LineChart";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { faChartLine, faChartSimple } from "@fortawesome/free-solid-svg-icons";
-
 function GraphPane() {
-  // const iconStyle: React.CSSProperties = { marginLeft: "0.4em" };
-
   return (
-    <Tabs>
-      {/* <TabList>
-        <Tab>
-          Hot Selling
-          <FontAwesomeIcon style={iconStyle} icon={faChartSimple} />
-        </Tab>
-        <Tab>
-          Sales
-          <FontAwesomeIcon style={iconStyle} icon={faChartLine} />
-        </Tab>
-      </TabList>
-      <TabPanel>
-        <SellingBarGraph />
-      </TabPanel>
-      <TabPanel>
-        <LineChart />
-      </TabPanel> */}
+    <>
       工事中
-    </Tabs>
+    </>
   );
 }
 
