f9a6fb719aa593634d6fab553e7c2803706b502c
Haur514 <haruka.yoshioka.514@gmail.com>
2023-05-15 17:00:46 +0000

    .envの環境変数をコンテナ内に通し，application.ymlから読み込めるようにしました

--- backend/src/main/java/jp/ac/osaka_u/ist/sdl/kjuiceapp/util/config/SlackConfig.java
+++ backend/src/main/java/jp/ac/osaka_u/ist/sdl/kjuiceapp/util/config/SlackConfig.java
@@ -2,12 +2,10 @@ package jp.ac.osaka_u.ist.sdl.kjuiceapp.util.config;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 @Data
 @Component
-@PropertySource("classpath:private.properties")
 @ConfigurationProperties(prefix = "slack")
 public class SlackConfig {
   private String token;
