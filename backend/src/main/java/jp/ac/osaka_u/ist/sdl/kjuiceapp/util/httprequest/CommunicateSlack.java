1589d7527745fd2562cfef5bde1056411f5d5294
Haur514 <haruka.yoshioka.514@gmail.com>
2023-05-17 13:39:00 +0000

    refactor

--- backend/src/main/java/jp/ac/osaka_u/ist/sdl/kjuiceapp/util/httprequest/CommunicateSlack.java
+++ backend/src/main/java/jp/ac/osaka_u/ist/sdl/kjuiceapp/util/httprequest/CommunicateSlack.java
@@ -1,17 +1,14 @@
 package jp.ac.osaka_u.ist.sdl.kjuiceapp.util.httprequest;
 
-import java.io.IOException;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
 import com.slack.api.Slack;
 import com.slack.api.methods.MethodsClient;
 import com.slack.api.methods.SlackApiException;
 import com.slack.api.methods.request.chat.ChatPostMessageRequest;
 import com.slack.api.methods.response.chat.ChatPostMessageResponse;
-
+import java.io.IOException;
 import jp.ac.osaka_u.ist.sdl.kjuiceapp.util.config.SlackConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 @Component
 public class CommunicateSlack {
@@ -23,10 +20,8 @@ public class CommunicateSlack {
     Slack slack = Slack.getInstance();
     MethodsClient methods = slack.methods(slackConfig.getToken());
 
-    ChatPostMessageRequest request = ChatPostMessageRequest.builder()
-      .channel(slackConfig.getChannel()) 
-      .text(message)
-      .build();
+    ChatPostMessageRequest request =
+        ChatPostMessageRequest.builder().channel(slackConfig.getChannel()).text(message).build();
 
     ChatPostMessageResponse response = methods.chatPostMessage(request);
   }
