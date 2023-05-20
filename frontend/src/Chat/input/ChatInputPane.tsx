9126a1b30a2e7571512a9bcf0ffac2fcf7ed0211
Haur514 <haruka.yoshioka.514@gmail.com>
2023-05-20 11:37:29 +0000

    チャットメッセージを入力した後，エンターで送信できるようにした

--- frontend/src/Chat/input/ChatInputPane.tsx
+++ frontend/src/Chat/input/ChatInputPane.tsx
@@ -13,8 +13,17 @@ function ChatInputPane({ postMessage }: Props) {
   }
 
   async function handlePost() {
+    if (message === "") {
+      return;
+    }
     await postMessage(message);
     setMessage("");
+  }
+
+  function pressEnter(event: React.KeyboardEvent<HTMLElement>) {
+    if (event.key === "Enter") {
+      handlePost();
+    }
   }
 
   return (
@@ -24,6 +33,7 @@ function ChatInputPane({ postMessage }: Props) {
           value={message}
           onChange={handleInputChange}
           placeholder="140字まで入力可能．それ以上は入力しないで．"
+          onKeyDown={pressEnter}
         />
         <Button type="submit" onClick={handlePost} colorScheme="teal">
           送信
