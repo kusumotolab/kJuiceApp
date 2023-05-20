454ecc12a18d4cd6f7d632e6bde29407f09746c0
grtw2116 <15563588+grtw2116@users.noreply.github.com>
2023-05-20 13:50:58 +0900

    ユーザ追加画面のバリデーションを実装 & レイアウト調整

--- frontend/src/Setting/Admin/UserAdd/UserAddPane.tsx
+++ frontend/src/Setting/Admin/UserAdd/UserAddPane.tsx
@@ -5,75 +5,92 @@ import {
   Select,
   Button,
   Stack,
+  FormHelperText,
+  FormErrorMessage,
+  Box,
 } from "@chakra-ui/react";
-import { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
 import { Backend } from "util/Backend";
+import { z } from "zod";
+
+interface IUserAddFormInput {
+  userId: string;
+  userName: string;
+  attribute: string;
+}
+
+const schema = z.object({
+  userId: z
+    .string()
+    .min(1, { message: "ユーザIDを入力してください" })
+    .regex(/^[a-z0-9_-]+$/, { message: "使用できない文字が含まれています" }),
+  userName: z.string().min(1, { message: "ユーザ名を入力してください" }),
+  attribute: z.string(),
+});
+type Schema = z.infer<typeof schema>;
 
 function UserAddPane() {
-  const [userId, setUserId] = useState("");
-  const [displayName, setDisplayName] = useState("");
-  const [attribute, setAttribute] = useState("teacher");
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+    reset,
+  } = useForm<Schema>({
+    resolver: zodResolver(schema),
+  });
 
-  async function addUser() {
-    if (!(await Backend.addMember(userId, displayName, attribute)))
+  async function onSubmit(data: IUserAddFormInput) {
+    addUser(data);
+    reset();
+  }
+
+  async function addUser(data: IUserAddFormInput) {
+    const { userId, userName, attribute } = data;
+    if (!(await Backend.addMember(userId, userName, attribute)))
       console.error("addUser: failed");
   }
 
-  function clearInput() {
-    setUserId("");
-    setDisplayName("");
-    setAttribute("teacher");
-  }
-
   return (
-    <Stack spacing={4}>
-      <FormControl id="icon">
-        <FormLabel>アイコン</FormLabel>
-      </FormControl>
-      <FormControl id="id">
-        <FormLabel>ID</FormLabel>
-        <Input
-          type="text"
-          name="name"
-          value={userId}
-          onChange={(event) => setUserId(event.target.value)}
-        />
-      </FormControl>
-      <FormControl id="displayName">
-        <FormLabel>表示名</FormLabel>
-        <Input
-          type="text"
-          value={displayName}
-          onChange={(event) => setDisplayName(event.target.value)}
-        />
-      </FormControl>
-      <FormControl id="attribute">
-        <FormLabel>属性</FormLabel>
-        <Select
-          name="new-user-attribute"
-          value={attribute}
-          onChange={(event) => setAttribute(event.target.value)}
-        >
-          <option value="teacher">先生</option>
-          <option value="m2">M2</option>
-          <option value="m1">M1</option>
-          <option value="b4">B4</option>
-        </Select>
-      </FormControl>
-      <Button
-        colorScheme="teal"
-        onClick={() => {
-          // デバッグ用
-          console.log(userId);
-          console.log(displayName);
-          console.log(attribute);
-          addUser();
-          clearInput();
-        }}
-      >
-        追加
-      </Button>
-    </Stack>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Stack spacing={4}>
+        <FormControl>
+          <FormLabel>アイコン（未実装）</FormLabel>
+        </FormControl>
+        <FormControl isInvalid={Boolean(errors.userId)}>
+          <FormLabel htmlFor="userId">ユーザID</FormLabel>
+          <Input id="userId" {...register("userId")} />
+          <FormHelperText>
+            利用可能な文字: 英字小文字 (a-z), 数字 (0-9), アンダーバー (_),
+            ハイフン (-)
+          </FormHelperText>
+          <FormErrorMessage>
+            {errors.userId && String(errors.userId.message)}
+          </FormErrorMessage>
+        </FormControl>
+        <FormControl isInvalid={Boolean(errors.userName)}>
+          <FormLabel htmlFor="userName">ユーザ名</FormLabel>
+          <Input id="userName" {...register("userName")} />
+          <FormErrorMessage>
+            {errors.userName && String(errors.userName.message)}
+          </FormErrorMessage>
+        </FormControl>
+        <FormControl>
+          <FormLabel>属性</FormLabel>
+          <Select id="attribute" {...register("attribute")}>
+            <option value="teacher">先生</option>
+            <option value="m2">M2</option>
+            <option value="m1">M1</option>
+            <option value="b4">B4</option>
+          </Select>
+        </FormControl>
+      </Stack>
+      <Box mt={8}>
+        <Button colorScheme="teal" isLoading={isSubmitting} type="submit">
+          追加
+        </Button>
+      </Box>
+    </form>
   );
 }
 
