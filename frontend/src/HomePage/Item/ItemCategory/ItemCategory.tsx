2b43c23cd2b5d8b8057928bc7107cbf9e56f5cef
grtw2116 <15563588+grtw2116@users.noreply.github.com>
2023-10-12 01:31:24 +0900

    useItems()変更にあわせて呼び出しを修正

--- frontend/src/HomePage/Item/ItemCategory/ItemCategory.tsx
+++ frontend/src/HomePage/Item/ItemCategory/ItemCategory.tsx
@@ -24,7 +24,7 @@ function ItemPerCategory({
   selectedItem,
   handleClickItemCard,
 }: Props) {
-  const items = useItems();
+  const { items } = useItems();
   const logoDictionary: LogoDictionary = {
     CocaCola: LogoCola,
     Fanta: LogoFanta,
