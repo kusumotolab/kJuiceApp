4631b90f74d0d1f61befe7fdead92596a9d70ad3
grtw2116 <15563588+grtw2116@users.noreply.github.com>
2023-10-12 01:29:09 +0900

    クリーンアップ関数を追加

--- frontend/src/hooks/useMemberImage.ts
+++ frontend/src/hooks/useMemberImage.ts
@@ -6,18 +6,30 @@ export default function useMemberImage(member: Member | null) {
   const [memberImage, setMemberImage] = useState("");
 
   useEffect(() => {
-    if (member === null) {
-      return;
+    let ignore = false;
+
+    async function fetchMemberImage() {
+      if (member === null) {
+        return;
+      }
+      Backend.getMemberImage(member.id)
+        .then((res) => {
+          if (res === null) {
+            console.error("getMemberImage: failed");
+            return;
+          }
+          if (ignore) {
+            return;
+          }
+          setMemberImage(URL.createObjectURL(res));
+        });
     }
-    Backend.getMemberImage(member.id)
-      .then((res) => {
-        if (res === null) {
-          console.error("getMemberImage: failed");
-          return;
-        }
-        setMemberImage(URL.createObjectURL(res));
-      });
-  }, [member]);
+    fetchMemberImage();
+
+    return () => {
+      ignore = true;
+    }
+  }, []);
 
   return memberImage;
 }
